d50b36a06f8b5a9f1d6ae608707593204efd6713
Alex Thewsey <athewsey@gmail.com>
2020-02-25 13:43:01 +0000

    fix(objdet): Add missing hosting import

---
+++ gt_object_det/src/sm_gluoncv_hosting.py
@@ -0,0 +1,44 @@
+"""Function defs for SageMaker MXNet container to host a GluonCV model
+
+This module exports functions which override the SM MXNet framework app defaults, allowing it to
+read and use a GluonCV model.
+"""
+
+# Python Built-Ins:
+import json
+
+# External Dependencies:
+import mxnet as mx
+
+
+def model_fn(model_dir):
+    """
+    Load the gluon model. Called once when hosting service starts.
+    :param: model_dir The directory where model files are stored.
+    :return: a model (in this case a Gluon network)
+    """
+    net = mx.gluon.SymbolBlock.imports(
+        '%s/model-symbol.json' % model_dir,
+        ['data'],
+        '%s/model-0000.params' % model_dir,
+    )
+    return net
+
+
+def transform_fn(net, data, input_content_type, output_content_type):
+    """
+    Transform a request using the Gluon model. Called once per request.
+    :param net: The Gluon model.
+    :param data: The request payload.
+    :param input_content_type: The request content type.
+    :param output_content_type: The (desired) response content type.
+    :return: response payload and content type.
+    """
+    # we can use content types to vary input/output handling, but
+    # here we just assume json for both
+    parsed = json.loads(data)
+    nda = mx.nd.array(parsed)
+    output = net(nda)
+    prediction = mx.nd.argmax(output, axis=1)
+    response_body = json.dumps(prediction.asnumpy().tolist()[0])
+    return response_body, output_content_type
